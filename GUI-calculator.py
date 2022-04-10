ab8947dcd766b78b0a6bd78afd771eb0762bc7f5
suplim2021 <75366665+suplim2021@users.noreply.github.com>
2022-04-10 11:55:32 +0700

    Update GUI-calculator.py

--- GUI-calculator.py
+++ GUI-calculator.py
@@ -2,51 +2,47 @@
 
 from tkinter import *
 from tkinter import ttk, messagebox
+from turtle import width
 
 GUI = Tk() # T ใหญ่ k เล็ก
-GUI.title('โปรแกรมคำนวณพื้นที่')
-GUI.geometry('700x600')
+GUI.title('โปรแกรมคำนวณหาพื้นที่')
+GUI.geometry('600x520')
 
-LM = Label(GUI,text='ปรแกรมคำนวณพื้นที่', font=('Sarabun',14))
+LM = Label(GUI,text='โปรแกรมคำนวณหาพื้นที่', font=('Sarabun',14))
 LM.pack()
 
 bg = PhotoImage(file='construction.png')
-#GUI.pack(padx=20, pady=20)
-
 BG = Label(GUI, image=bg)
 BG.pack()
 
-
-L1 = Label(GUI,text='กรอกความกว้าง (เมตร)', font=('Sarabun',14))
+L1 = Label(GUI,text='ความกว้าง (เมตร)', font=('Sarabun',14))
 L1.pack()
 
-v_quantity = StringVar()
-E1 = ttk.Entry(GUI, textvariable=v_quantity, font=('Sarabun',12))
+width_v = StringVar() #ตัวแปรเก็บความกว้าง
+E1 = ttk.Entry(GUI, textvariable=width_v, font=('Sarabun',12))
 E1.pack()
 
-L2 = Label(GUI,text='กรอกความยาว (เมตร)', font=('Sarabun',14))
+L2 = Label(GUI,text='ความยาว (เมตร)', font=('Sarabun',14))
 L2.pack()
 
-v2_quantity = StringVar()
-E2 = ttk.Entry(GUI, textvariable=v2_quantity, font=('Sarabun',12))
+length_v = StringVar() #ตัวแปรเก็บความยาว
+E2 = ttk.Entry(GUI, textvariable=length_v, font=('Sarabun',12))
 E2.pack()
 
 def Cal():
-    try:
-        quan = float(v_quantity.get())
-        f1 = quan
-        quan = float(v2_quantity.get())
-        f2 = quan
-        calc = f1 * f2
+    try: # คำนวณ
+        width = float(width_v.get())
+        length = float(length_v.get())
+        calc = width * length # กว้าง * ยาว
         messagebox.showinfo('ผลลัพธ์','มีพื้นที่ {} ตารางเมตร'.format(calc))
-        E1.focus()
-    except:
+        E1.focus() # โฟกัสที่ช่องสำหรับกรอกข้อความช่องแรก
+    except: # ผิดพลาด
         messagebox.showinfo('ผิดพลาด','กรุณากรอกเฉพาะตัวเลขเท่านั้น')
-        v_quantity.set('')
-        v2_quantity.set('')
-        E1.focus()
+        width_v.set('') # ล้างค่าช่อง width_v
+        length_v.set('') # ล้างค่าช่อง length_v
+        E1.focus() # โฟกัสที่ช่องสำหรับกรอกข้อความช่องแรก
           
-B = ttk.Button(GUI, text='คำนวณ', command=Cal)
-B.pack(ipadx=10,ipady=10)
+B = ttk.Button(GUI, text='คำนวณ', command=Cal) #เมื่อกดปุ่ม ใช้ Cal
+B.pack(ipadx=10,ipady=10) # padx 10, pady 10
 
 GUI.mainloop() # เพื่อให้โปรแกรมรันตลอดเวลา
